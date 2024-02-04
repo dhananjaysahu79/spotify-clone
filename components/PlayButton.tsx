46b28d58cae39fcba5678f8a982432568c9101c6
dhananjaysahu79 <dhananjaysahu79@gmail.com>
2024-02-05 02:04:12 +0530

    Added playbutton

---
+++ components/PlayButton.tsx
@@ -0,0 +1,26 @@
+import { FaPlay } from "react-icons/fa";
+const PlayButton = () => {
+    return (
+        <button 
+        className="
+          absolute 
+          transition 
+          opacity-0 
+          rounded-full 
+          flex 
+          items-center 
+          justify-center 
+          bg-green-500 
+          p-4 
+          drop-shadow-md 
+          right-5
+          group-hover:opacity-100 
+          hover:scale-110
+        "
+      >
+        <FaPlay className="text-black" />
+      </button>
+    );
+}
+
+export default PlayButton;
